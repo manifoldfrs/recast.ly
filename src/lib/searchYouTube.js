179cecad7ad67633d201e3e6dae46c97c9136eaa
Faris Habib <faris.habib.7@gmail.com>
2018-09-30 17:28:39 -0700

    Working on API helper

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,27 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=',
+    type: 'GET',
+    dataType: 'json',
+    key: window.YOUTUBE_API_KEY,
+    query: 'string',
+    part: 'snippet',
+    videoEmbeddable: true,
+    max: 5,
+    success: () => {
+      console.log("Successfully received data!");
+    },
+    fail: () => {
+      console.log('Error:', data);
+    }
+
+
+
+
+  }).done((data) => {
+    callback(data.items);
+  });
 };
 
 window.searchYouTube = searchYouTube;
